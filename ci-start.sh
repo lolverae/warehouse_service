d60dd605c1200ac34f651a49ef3d11fed7451e23
Cloud User <alberto@minikube.xb021jsnmrvexhtg3dd1xrunpf.jx.internal.cloudapp.net>
2021-08-11 20:29:02 +0000

    improved ansible

--- ci-start.sh
+++ ci-start.sh
@@ -3,10 +3,7 @@
 # 1.0.1 
 # append commit id
 # create job, run to generate new docker tag, 
-# change based on the commit id, crear archivo en python con la version y aumentar la version, 
 # desde 1.0.0. ej,
-# 
-# $(ansible playbook -e="image_name=sadln" -e "{})
 
 
 #echo "Enter a name for the docker image: "
@@ -15,5 +12,5 @@
 COMMIT_ID=$(git rev-parse --verify HEAD | cut -f3) 
 
 IMAGE_VERSION=$(cat version.env)-$(cut -c1-6 <<< $COMMIT_ID)
-echo $IMAGE_VERSION
-$(ansible-playbook -i -e image_name=lolverae/warehouse_service -e image_version=$IMAGE_VERSION)
+echo "version: $IMAGE_VERSION"
+ansible-playbook -e "image_name=lolverae/warehouse_service" -e "image_version=$IMAGE_VERSION" ansible/main.yml
