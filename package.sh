5e10afa6293c5853a78a931af3f5fb27b507a64e
lolverae <lolverae1300@alumno.ipn.mx>
2021-08-24 11:43:42 -0500

    lkol

--- package.sh
+++ package.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #echo "Enter a name for the docker image: "
 #read IMAGE_NAME
-
+whoami
 COMMIT_ID=$(git rev-parse --verify HEAD | cut -f3) 
 
 IMAGE_VERSION=$(cat version.env)-$(cut -c1-6 <<< $COMMIT_ID)
