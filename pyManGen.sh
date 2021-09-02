dd89f3111a4b54728683a01dfd2b82037c6fae3e
lolverae <lolverae1300@alumno.ipn.mx>
2021-09-02 03:29:57 -0500

    added script to generate manifest

---
+++ pyManGen.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+$(git clone https://github.com/lolverae/genManifest)
+COMMIT_ID=$(git rev-parse --verify HEAD | cut -f3) 
+IMAGE_VERSION=$(cat version.env)-$(cut -c1-6 <<< $COMMIT_ID)
+cd genManifest
+python3 genManifest.py warehouse $IMAGE_VERSION
