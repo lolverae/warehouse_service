cad50da27f3d06bdefb1441661099521ce733432
lolverae <lolverae1300@alumno.ipn.mx>
2021-09-02 09:00:08 -0500

    fixed pipeline errors

--- pyManGen.sh
+++ pyManGen.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
 COMMIT_ID=$(git rev-parse --verify HEAD | cut -f3) 
 IMAGE_VERSION=$(cat version.env)-$(cut -c1-6 <<< $COMMIT_ID)
-python3 /home/alberto/genManifest/genManifest.py warehouse $IMAGE_VERSION
+cd ~/genManifest
+python3 genManifest.py warehouse $IMAGE_VERSION
