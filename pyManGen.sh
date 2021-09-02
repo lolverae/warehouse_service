f59e39f3eab23bf348e7471fae9e0bd783825fb8
lolverae <lolverae1300@alumno.ipn.mx>
2021-09-02 08:55:16 -0500

    removed relative folders

--- pyManGen.sh
+++ pyManGen.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-$(git clone https://github.com/lolverae/genManifest)
 COMMIT_ID=$(git rev-parse --verify HEAD | cut -f3) 
 IMAGE_VERSION=$(cat version.env)-$(cut -c1-6 <<< $COMMIT_ID)
-cd genManifest
-python3 genManifest.py warehouse $IMAGE_VERSION
-sleep 10
-cd .. && rm -rf genManifest
+python3 /home/alberto/genManifest/genManifest.py warehouse $IMAGE_VERSION
