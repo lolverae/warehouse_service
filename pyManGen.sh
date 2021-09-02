a514dee46af4067b97a2f20d357a7f27a26b96bb
lolverae <lolverae1300@alumno.ipn.mx>
2021-09-02 08:32:58 -0500

    add cleanup

--- pyManGen.sh
+++ pyManGen.sh
@@ -4,3 +4,5 @@ COMMIT_ID=$(git rev-parse --verify HEAD | cut -f3)
 IMAGE_VERSION=$(cat version.env)-$(cut -c1-6 <<< $COMMIT_ID)
 cd genManifest
 python3 genManifest.py warehouse $IMAGE_VERSION
+sleep 10
+cd .. && rm -rf genManifest
