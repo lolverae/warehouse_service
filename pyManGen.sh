e385fa20a7a8776f0f7a8f6f5e042ae1d4e18556
lolverae <lolverae1300@alumno.ipn.mx>
2021-09-02 09:05:47 -0500

    fix to jenkins path

--- pyManGen.sh
+++ pyManGen.sh
@@ -2,5 +2,4 @@
 
 COMMIT_ID=$(git rev-parse --verify HEAD | cut -f3) 
 IMAGE_VERSION=$(cat version.env)-$(cut -c1-6 <<< $COMMIT_ID)
-cd ~/genManifest
-python3 genManifest.py warehouse $IMAGE_VERSION
+cd /home/alberto/genManifest && python3 genManifest.py warehouse $IMAGE_VERSION 
